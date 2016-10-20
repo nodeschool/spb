49b01a88a5bdd1df1a25b7fe5b73683d57d0d01c
Igor Pelekhan <igor@pelekhan.com>
2016-10-20 22:52:04 +0300

    :boom: Автоматический deploy через Travis-CI #32

---
+++ deploy-to-gh-pages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit
+
+git config --global user.email "spb@nodeschool.io"
+git config --global user.name "Almighty Travis CI"
+
+cd public
+git init
+git add .
+git commit -m ":turtle: Cowabunga, dude!"
+git remote set-url origin git@github.com:nodeschool/spb.git
+git push --force --quiet origin master:gh-pages > /dev/null 2>&1
