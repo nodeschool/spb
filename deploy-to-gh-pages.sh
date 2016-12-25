937ac79f1f48e3b2e2dd15ceb4e29e1a97a7cd2c
Igor Pelekhan <igor@pelekhan.com>
2016-12-26 00:33:43 +0300

    :boom: Update Travis CI commit message #32

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -7,6 +7,6 @@ git config --global user.name "Almighty Travis CI"
 cd public
 git init
 git add .
-git commit -m ":turtle: Cowabunga, dude!"
+git commit -m ":cake: $COMMIT_MESSAGE; $AUTHOR_NAME; $COMMIT_DATE"
 git remote add origin git@github.com:nodeschool/spb.git
 git push --force --quiet origin master:gh-pages > /dev/null 2>&1
